fcd6a27a795fd7a7b60e1fd4a2b162dd670457ef
Valdez, Ramoncito Jr. T. <valdez.ramoncitojr.071502@gmail.com>
2025-09-15 20:19:26 +0800

    Seatwork #1 Separated into 2 Files

---
+++ SW1/SW1-CurrencyConvert/SW1-CurrencyConvert/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW1_CurrencyConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> money = new List<double>();
+            Console.WriteLine("Currency Converter Program");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Enter currency in ($): ");
+                money.Add(Convert.ToDouble(Console.ReadLine()));
+            }
+
+            Console.WriteLine();
+            dollarToPesoToYen(money);
+
+            Console.ReadKey();
+        }
+        //converts the dollar to peso and yen
+        static void dollarToPesoToYen(List<double> money)
+        {
+            Console.WriteLine("Dollar ($)\tPhil Peso (P)\tJpn Yen (Y)");
+            foreach (double dollar in money)
+            {
+                Console.WriteLine(dollar + "\t\t" + dollar * 57.17 + "\t\t" + dollar * 146.67);
+            }
+        }
+    }
+}
---
+++ SW1/SW1-PrimeorComposite/SW1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime or Composite Program");
+            List <String> input = new List <String>();
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write("Enter a String: ");
+                input.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            isPrimeorComposite(input);
+
+            Console.ReadKey();
+        }
+        //Checks if its Prime or Composite
+        static void isPrimeorComposite(List <String> inputs)
+        {
+            int counter = 1;
+            foreach (String input in inputs)
+            {
+                bool isPrime = true;
+                int num = int.Parse(input);
+                Console.Write($"{counter}. ");
+                if (num <= 1)
+                {
+                    Console.WriteLine($"{num} is neither Prime or Composite");
+                    continue;
+                }
+                else
+                {
+                    for (int i = 2; i * i <= num; i++)
+                    {
+                        if (num % i == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        Console.WriteLine($"{num}\tPrime");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{num}\tComposite");
+                    }
+                }
+                counter++;
+            }
+        }
+    }
+}
